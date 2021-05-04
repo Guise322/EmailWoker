8949db206cd3fa77d678bd3075bfbd32b4d0a5db
Dmitry <guise322@ya.ru>
2021-05-04 13:23:17 +0300

    Add a method that gets inbox messages from Gmail
    A list of messages is got by means of IMAP server.

--- Worker.cs
+++ Worker.cs
@@ -5,12 +5,16 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using IpByEmail.MailRepository;
 
 namespace IpByEmail
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        //dimsonartex@gmail.com
+        private MailRepositoryClass _mailRepositoryClass = new MailRepositoryClass("imap.gmail.com", 993, 
+            true, "dimsonartex@gmail.com", "17890714");
 
         public Worker(ILogger<Worker> logger)
         {
@@ -19,11 +23,23 @@ namespace IpByEmail
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            IEnumerable<string> unreadMails;
             while (!stoppingToken.IsCancellationRequested)
             {
+                try
+                {
+                    unreadMails = _mailRepositoryClass.GetUnreadMails();
+                }
+                catch (System.Exception e)
+                {
+                    if(e != null)
+                        _logger.LogError(e.Message);
+                    else
+                        _logger.LogInformation("Worker is over for an error at {time}", DateTimeOffset.Now);
+                }
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                 await Task.Delay(1000, stoppingToken);
             }
         }
-    }
+    }   
 }
