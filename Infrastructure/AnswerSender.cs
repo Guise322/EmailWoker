96d36a95d06a13fdcd7bb67ba2ad416f6766fd44
Dmitry <guise322@ya.ru>
2021-09-04 10:34:54 +0300

    Implement the logging in the methods and refactor the logic of searching a requesting message

--- Infrastructure/AnswerSender.cs
+++ Infrastructure/AnswerSender.cs
@@ -1,12 +1,18 @@
 using EmailWorker.ApplicationCore.Entities;
 using EmailWorker.ApplicationCore.Interfaces;
 using MailKit.Net.Smtp;
+using Microsoft.Extensions.Logging;
 using MimeKit;
 
 namespace EmailWorker.Infrastructure
 {
     public class AnswerSender : IAnswerSender
     {
+        private readonly ILogger _logger;
+        public AnswerSender(ILogger<AnswerSender> logger)
+        {
+            _logger = logger;
+        }
         public void SendAnswerBySmtp(MimeMessage message, EmailCredentials emailCredentials)
         {
             int smtpPort = 465;
@@ -15,6 +21,8 @@ namespace EmailWorker.Infrastructure
             client.Connect(emailCredentials.MailServer, smtpPort, emailCredentials.Ssl);
             client.Authenticate(emailCredentials.Login, emailCredentials.Password);
             client.Send(message);
+
+            _logger.LogInformation($"The answer is sended from the {emailCredentials.Login} inbox.");
         }
     }
 }
--- Infrastructure/EmailCredentialsGetter.cs
+++ Infrastructure/EmailCredentialsGetter.cs
@@ -3,18 +3,27 @@ using System.IO;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using EmailWorker.ApplicationCore.Entities;
+using Microsoft.Extensions.Logging;
 
 namespace EmailWorker.Infrastructure
 {
     public class EmailCredentialsGetter
     {
-        public static List<EmailCredentials> GetEmailCredentials()
+        private readonly ILogger _logger;
+        public EmailCredentialsGetter(ILogger<EmailCredentialsGetter> logger)
+        {
+            _logger = logger;
+        }
+        public List<EmailCredentials> GetEmailCredentials()
         {
             JsonStringEnumConverter stringEnumConverter = new();
             JsonSerializerOptions opts = new();
             opts.Converters.Add(stringEnumConverter);
             string jsonString = File.ReadAllText("EmailCredentials.json");
-            return JsonSerializer.Deserialize<List<EmailCredentials>>(jsonString, opts);
+            List<EmailCredentials> emailCredentials = 
+                JsonSerializer.Deserialize<List<EmailCredentials>>(jsonString, opts);
+            _logger.LogInformation("EmailCredentials is got.");
+            return emailCredentials;
         }
     }
 }
