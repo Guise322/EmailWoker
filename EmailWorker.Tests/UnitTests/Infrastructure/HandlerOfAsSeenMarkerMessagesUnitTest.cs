3ff762b13ee8359c0660e9be3ff280cda308fbba
Dmitry <guise322@ya.ru>
2022-01-26 21:25:44 +0300

    Write out unit tests for the HandlerOfAsSeenMarkerMessages class

--- EmailWorker.Tests/UnitTests/Infrastructure/HandlerOfAsSeenMarkerMessagesUnitTest.cs
+++ EmailWorker.Tests/UnitTests/Infrastructure/HandlerOfAsSeenMarkerMessagesUnitTest.cs
@@ -1,4 +1,5 @@
 using EmailWorker.Infrastructure.HandlersOfProcessedMessages;
+using EmailWorker.Tests.UnitTests.Shared;
 using MailKit;
 using Microsoft.Extensions.Logging;
 using Moq;
@@ -23,12 +24,31 @@ public class HandlerOfAsSeenMarkerMessagesUnitTest
     [Fact]
     public void HandlerProcessedMessages_NumberOfMessagesIDsAboveMaxLimit_NullNull()
     {
-
+        Mock<ILogger<HandlerOfAsSeenMarkerMessages>> loggerStub = new();
+        Mock<IMailStore> mailStoreStub = new();
+        HandlerOfAsSeenMarkerMessages handler = new(loggerStub.Object, mailStoreStub.Object);
+        Mock<IMailFolder> inboxStub = new();
+        mailStoreStub.SetupGet(p => p.Inbox).Returns(inboxStub.Object);
+        int numberAboveMaxLimit = 1000;
+        var uniqueIDsShim = UniqueIDsShim.Create(numberAboveMaxLimit);
+        (var actualEmailText, var actualEmailSubject) = handler.HandleProcessedMessages(uniqueIDsShim);
+        Assert.Equal((null, null),(actualEmailText, actualEmailSubject));
     }
 
     [Fact]
     public void HandlerProcessedMessages_NumberOfMessageIDsBelowMaxLimit_NumberOfProcessedMessaggesAndInfoString()
     {
-
+        Mock<ILogger<HandlerOfAsSeenMarkerMessages>> loggerStub = new();
+        Mock<IMailStore> mailStoreStub = new();
+        HandlerOfAsSeenMarkerMessages handler = new(loggerStub.Object, mailStoreStub.Object);
+        Mock<IMailFolder> inboxStub = new();
+        mailStoreStub.SetupGet(p => p.Inbox).Returns(inboxStub.Object);
+        int numberAboveMaxLimit = 999;
+        var uniqueIDsShim = UniqueIDsShim.Create(numberAboveMaxLimit);
+        (var actualEmailText, var actualEmailSubject) = handler.HandleProcessedMessages(uniqueIDsShim);
+        Assert.Equal(
+            (numberAboveMaxLimit.ToString(),"The count of messages marked as seen"),
+            (actualEmailText, actualEmailSubject)
+        );
     }
 }
